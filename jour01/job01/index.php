260dd3c42afa4f7fea00d4442ba95cf187dabddb
Maxence Le Brun <115214683+maxenceLe-brun@users.noreply.github.com>
2023-05-22 13:26:07 +0200

    Create index.php
    learned .php uses

---
+++ jour01/job01/index.php
@@ -0,0 +1,11 @@
+<!doctype html>	
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Titre de la page</title>
+  <script>window.alert("Hello World!");</script>
+</head>
+<body>
+  
+</body>
+</html>
