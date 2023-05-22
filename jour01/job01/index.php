a7f536deb26cf8e07e25147d386379db8c958f8c
Maxence Le Brun <115214683+maxenceLe-brun@users.noreply.github.com>
2023-05-22 13:32:49 +0200

    Update index.php
    changed "world" by "javascirpt"
    miss-readed

--- jour01/job01/index.php
+++ jour01/job01/index.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <title>Titre de la page</title>
-  <script>window.alert("Hello World!");</script>
+  <script>window.alert("Hello JavaScript!");</script>
 </head>
 <body>
   
