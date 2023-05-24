991ee42b0d02b2d2a669c328be643e410da624e6
Maxence Le Brun <115214683+maxenceLe-brun@users.noreply.github.com>
2023-05-24 16:23:58 +0200

    Create script.js

---
+++ jour01/job05/script.js
@@ -0,0 +1,6 @@
+function afficherjourssemaines() {
+  let jourssemaines = ["lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi", "dimanche"];
+  for (let i = 0; i < 7; i++) {
+    console.log(jourssemaines[i]);
+  }
+}
