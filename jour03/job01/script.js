4109d9202fafc3c3bd1164ac614e1d4a5ce63437
Maxence Le Brun <115214683+maxenceLe-brun@users.noreply.github.com>
2023-05-25 16:26:15 +0200

    Create script.js

---
+++ jour03/job01/script.js
@@ -0,0 +1,6 @@
+function Show() {
+    document.getElementById("demo").innerHTML = "Les logiciels et les cathédrales, c'est un peu la même chose - d'abord on les construit, ensuite on prie.";
+}
+function Hide() {
+    document.getElementById("demo").innerHTML = ""
+}
